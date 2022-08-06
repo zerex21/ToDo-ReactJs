db53f00effec020dea0c2ee493063de2c95f1431
oleg <zerex21@icloud.com>
2022-08-06 18:14:35 +0300

    feat: lesson 1

---
+++ todo-app/src/components/List/List.jsx
@@ -0,0 +1,20 @@
+/* import listSvg from "../../src/assets/img/list.svg";
+ */
+
+import "./List.scss"
+const List = (props) => {
+    /* debugger */
+  return (
+         <ul className="list">
+          {props.items.map((item) =>(
+            <li className={item.active ? "active" : ''}>
+            <i>{item.icon ? item.icon : <i className={`badge badge--${item.color}`}></i>}</i>
+            <span>{item.name}</span>
+          </li>)
+        )
+      }
+      </ul>
+  );
+};
+
+export default List
